4424c7f4ba61cf28907a4ae8e39180eea638e103
Najamuddin Baig <NMBAIG@OUTLOOK.COM>
2014-06-19 12:14:46 -0700

    Programming Assignment for Coursera Course RProgramming - Assignment #2 Submitted by Najamuddin Baig nmbaig@outlook.com

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## creates and sets the values of a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInv <- function(solve) m <<- solve
+        getInv <- function() m
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
-
+## creates the inverse of a matrix, if exists, gets from cache and if missing calculates it and puts it in cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		m <- x$getInv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInv(m)
+        m		
 }
